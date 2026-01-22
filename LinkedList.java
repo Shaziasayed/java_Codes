17f300cfd4d1bed2c09dbe8659bda3b1e711d28c
Shaziasayed <sayedshazia070@gmail.com>
2026-01-22 22:36:53 +0530

    adding nodes

---
+++ LinkedList.java
@@ -0,0 +1,35 @@
+public class LinkedList {
+    Node head;
+    class Node{
+        String data;
+        Node next;
+        Node(String data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public  void addFirst(String data){
+        Node newnode=new Node(data);
+
+        if (head==null){
+            head=newnode;
+            return ;        
+        }
+        newnode.next=head;
+        head=newnode;
+    }
+    public  void print(){
+        Node currNode=head;
+        while(currNode!=null){
+            System.out.print(currNode.data+"-->");
+            currNode=currNode.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String args[]){
+        LinkedList list=new LinkedList();
+        list.addFirst("This");
+        list.addFirst("is");
+        list.print();
+    }
+}
--- ReverseofSinglyLinkedList.java
+++ ReverseofSinglyLinkedList.java
@@ -131,3 +131,128 @@ list.reverseIterate();
 list.printList();
 }
 }
+ public class SinglyLinkedList {
+// Node head;
+// private int size;
+
+// SinglyLinkedList() {
+// this.size = 0;
+// }
+
+// class Node {
+// String data;
+// Node next;
+
+// Node(String data) {
+// this.data = data;
+// this.next = null;
+// size++;
+// }
+// }
+
+// public void addFirst(String data) {
+// Node newNode = new Node(data);
+
+// if (head == null) {
+// head = newNode;
+// return;
+// }
+
+// newNode.next = head;
+// head = newNode;
+// }
+
+// public void addLast(String data) {
+// Node newNode = new Node(data);
+// if (head == null) {
+// head = newNode;
+// return;
+// }
+
+// Node currNode = head;
+// while (currNode.next != null) {
+// currNode = currNode.next;
+// }
+
+// currNode.next = newNode;
+// }
+
+// public void printList() {
+// if (head == null) {
+// System.out.println("List is empty");
+// return;
+// }
+
+// Node currNode = head;
+// while (currNode != null) {
+// System.out.print(" " + currNode.data + " " + "-->" + " ");
+// currNode = currNode.next;
+// }
+
+// System.out.println("Null");
+// }
+
+// public void deleteFirst() {
+// if (head == null) {
+// System.out.println("This list is empty");
+// return;
+// }
+// size--;
+// head = head.next;
+// }
+
+// public void deleteLast() {
+// if (head == null) {
+// System.out.println("This list is empty");
+// return;
+// }
+// size--;
+
+// if (head.next == null) {
+// head = null;
+// return;
+// }
+
+// Node secondLast = head;
+// Node lastNode = head.next;
+// while (lastNode.next != null) {
+// lastNode = lastNode.next;
+// secondLast = secondLast.next;
+// }
+
+// secondLast.next = null;
+// }
+
+// public int getSize() {
+// return size;
+// }
+
+// // Method to reverse a LinkedList.
+// public Node reverseRecursive(Node head) {
+// // Base case
+// if (head == null || head.next == null) {
+// return head;
+// }
+
+// Node newHead = reverseRecursive(head.next);
+// head.next.next = head;
+// head.next = null;
+
+// return newHead;
+// }
+
+// public static void main(String args[]) {
+// SinglyLinkedList list = new SinglyLinkedList();
+// list.addFirst("a");
+// list.addFirst("is");
+// list.addFirst("This");
+// list.printList();
+
+// list.addLast("list");
+// list.printList();
+
+// list.head = list.reverseRecursive(list.head);
+// list.printList();
+// }
+// }
+/*---------------------------------------------------------------- */
